01560c746bb926bb2478239aeae4a686911add28
RenanLucenaDosSantos <renan@codefabrik.dev>
2024-04-04 10:06:24 -0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularPortfolio';
+export class AppComponent implements OnInit {
+  activeMobileMenu = false
+  
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+  toggleMobileMenu() {
+    this.activeMobileMenu = !this.activeMobileMenu
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
